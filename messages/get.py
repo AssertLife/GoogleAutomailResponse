e906397fbab46d54bb46658d61bc54af752a2ab3
AssertLife <edennimni34@gmail.com>
2017-07-29 17:03:14 +0300

    Update get.py

--- messages/get.py
+++ messages/get.py
@@ -1,7 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 # mformat (string) - The format to return the message in. (full/metadata/minimal/raw)
 # mmetadataHeaders (string) - When given and format is METADATA, only include headers specified.
 # https://developers.google.com/gmail/api/v1/reference/users/messages/get
 def GetMessage(
-	service, user_id, mid,
-	mformat='full', mmetadataHeaders=''):
-    return service.users().messages().get(userId=user_id, id=mid, format=mformat, metadataHeaders=mmetadataHeaders).execute()
+    service,
+    user_id,
+    mid,
+    mformat='full',
+    mmetadataHeaders='',
+    ):
+
+    return service.users().messages().get(userId=user_id, id=mid,
+            format=mformat, metadataHeaders=mmetadataHeaders).execute()
